cc5a41eff42741359de061951c62cfb2779ca5d3
Ardani Ramirez <ardaniramirez@gmail.com>
2017-02-21 16:11:41 -0600

    formulario de pago y nuevopago

---
+++ PV/Vistas/movimientos/FrmNuevoPago.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PV.Vistas.movimientos
+{
+    partial class FrmNuevoPago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label3.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1019, 34);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nuevo Pago";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmNuevoPago
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 502);
+            this.Controls.Add(this.label3);
+            this.Name = "FrmNuevoPago";
+            this.Text = "FrmNuevoPago";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ PV/Vistas/movimientos/FrmPago.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PV
+{
+    public partial class FrmPago : Form
+    {
+        public FrmPago()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPago_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
